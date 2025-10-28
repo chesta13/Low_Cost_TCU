c031f8edd788ff0a16a11135f9598c3f50a1a272
Chesta Agnihotri <114438600+chesta13@users.noreply.github.com>
2025-10-28 16:58:39 +0530

    Delete drivers directory

--- drivers/i2c_driver.c
+++
@@ -1,8 +0,0 @@
-#include "i2c_driver.h"
-#include "ti_msp_dl_config.h"
-#include <ti/driverlib/dl_i2c.h>
-
-void I2C_Init(void)
-{
-    SYSCFG_DL_I2C_0_init();
-}
--- drivers/i2c_driver.h
+++
@@ -1,6 +0,0 @@
-#ifndef I2C_DRIVER_H_
-#define I2C_DRIVER_H_
-
-void I2C_Init(void);
-
-#endif
