04624a8996b1adef5a01d5017eb7940a95052a93
Chesta Agnihotri <114438600+chesta13@users.noreply.github.com>
2025-10-28 16:58:29 +0530

    Delete core directory

--- core/app_tasks.h
+++
@@ -1,6 +0,0 @@
-#ifndef APP_TASKS_H_
-#define APP_TASKS_H_
-
-void App_Task_Loop(void);
-
-#endif
--- core/system_init.h
+++
@@ -1,6 +0,0 @@
-#ifndef SYSTEM_INIT_H_
-#define SYSTEM_INIT_H_
-
-void System_Init(void);
-
-#endif
